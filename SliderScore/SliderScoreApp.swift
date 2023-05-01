94256a7209c8ddc570b5bf24e80dec6a34923272
Anton Kuzmin <furthertwoant@gmail.com>
2023-05-01 10:56:56 +0400

    Initial Commit

---
+++ SliderScore/SliderScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderScoreApp.swift
+//  SliderScore
+//
+//  Created by Anton Kuzmin on 01.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
