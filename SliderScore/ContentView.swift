82ba36e034c6d1dbfc2e5f9d6b25d4aa9dfa62a6
Anton Kuzmin <furthertwoant@gmail.com>
2023-05-01 20:07:45 +0400

    Implemented mini game

--- SliderScore/ContentView.swift
+++ SliderScore/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var targetValue = Int.random(in: 1...99)
+    @State private var sliderValue = Float.random(in: 1...99)
+    @State private var isAlertPresented = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text("Подвиньте слайдер как можно ближе к \(targetValue)")
+            
+            HStack {
+                Text("0")
+                UISliderViewRepresentation(
+                    guessedValue: $sliderValue,
+                    targetValue: targetValue
+                )
+                Text("100")
+            }
+            
+            Button("Проверь меня!", action: {isAlertPresented.toggle()})
+                .alert("Your score", isPresented: $isAlertPresented, actions: {}) {
+                    Text(computeScore().formatted())
+                }
+            
+            Button("Начать заново") {
+                targetValue = Int.random(in: 1...99)
+                sliderValue = Float.random(in: 1...99)
+            }
         }
         .padding()
+    }
+    
+    private func computeScore() -> Int {
+        let difference = abs(targetValue - lround(Double(sliderValue)))
+        return 100 - difference
     }
 }
 
---
+++ SliderScore/UISliderViewRepresentation.swift
@@ -0,0 +1,70 @@
+//
+//  UISliderViewRepresentation.swift.swift
+//  SliderScore
+//
+//  Created by Anton Kuzmin on 01.05.2023.
+//
+
+import SwiftUI
+
+struct UISliderViewRepresentation: UIViewRepresentable {
+    @Binding var guessedValue: Float
+    
+    let targetValue: Int
+    
+    func makeUIView(context: Context) -> UISlider {
+        let slider = UISlider()
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        slider.thumbTintColor = .red
+        
+        slider.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.didSliderChanged),
+            for: .valueChanged
+        )
+        
+        return slider
+    }
+    
+    func updateUIView(_ uiView: UISlider, context: Context) {
+        uiView.value = guessedValue
+        
+        uiView.thumbTintColor = uiView.thumbTintColor?.withAlphaComponent(CGFloat(calculateAlpha()))
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(guessedNumber: $guessedValue)
+    }
+    
+    private func calculateAlpha() -> Float {
+        let difference = Float(targetValue - Int(guessedValue))
+        
+        if difference == 0 {
+            return 1
+        } else {
+            return Float(1 - abs(difference / 100))
+        }
+    }
+}
+
+// MARK: - Coordinator
+extension UISliderViewRepresentation {
+    final class Coordinator: NSObject {
+        @Binding var guessedNumber: Float
+
+        init(guessedNumber: Binding<Float>) {
+            self._guessedNumber = guessedNumber
+        }
+        
+        @objc func didSliderChanged(_ sender: UISlider) {
+            guessedNumber = sender.value
+        }
+    }
+}
+
+struct UISliderViewRepresentation_Previews: PreviewProvider {
+    static var previews: some View {
+        UISliderViewRepresentation(guessedValue: .constant(20), targetValue: 99)
+    }
+}
